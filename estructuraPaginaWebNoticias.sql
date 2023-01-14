307dda6bf20866b1e549e45940204093d4b57ae1
Leonel Chagoya <hugoleovazquez@gmail.com>
2023-01-14 10:33:14 -0600

    se agrega la estructura del ejercicio dos

---
+++ estructuraPaginaWebNoticias.sql
@@ -0,0 +1,47 @@
+CREATE TABLE Usuarios (
+    idusuario INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    correo VARCHAR(255) NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    contraseña VARCHAR(255) NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    es_interno BIT NOT NULL
+);
+GO
+
+
+CREATE TABLE Personal (
+    idpersonal INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    apepaterno VARCHAR(255) NOT NULL,
+    apematerno VARCHAR(255) NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    direccion VARCHAR(255) NOT NULL,
+    fechadeingreso DATETIME
+);
+GO
+
+CREATE TABLE Notas (
+    idnota INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    idpersonal INT FOREIGN KEY REFERENCES Personal(idpersonal) NOT NULL,
+    contenido TEXT NOT NULL,
+    fecha_publicacion DATETIME NOT NULL
+);
+GO
+CREATE TABLE Comentarios (
+    idcomentario INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    idnota INT FOREIGN KEY REFERENCES Notas(idnota) NOT NULL,
+    idusuario INT FOREIGN KEY REFERENCES Usuarios(idusuario) NOT NULL,
+    contenido TEXT NOT NULL,
+    fecha_comentario DATETIME NOT NULL,
+    es_interno BIT NOT NULL
+);
+GO
+CREATE TABLE Respuestas (
+    idrespuesta INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    idcomentario INT FOREIGN KEY REFERENCES Comentarios(idcomentario) NOT NULL,
+    idusuario INT FOREIGN KEY REFERENCES Usuarios(idusuario) NOT NULL,
+    contenido TEXT NOT NULL,
+    fecha_respuesta DATETIME NOT NULL,
+    es_interno BIT NOT NULL
+);
+
+GO
